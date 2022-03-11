07774dab678f934aad6809fd3216256e3afd5619
Celezt <59172226+Celezt@users.noreply.github.com>
2022-03-11 14:06:14 +0100

    Added trim username when creating a new user

--- src/main/java/client/controllers/NewUserController.java
+++ src/main/java/client/controllers/NewUserController.java
@@ -31,6 +31,11 @@ public class NewUserController extends SceneControllerParent implements Initiali
     @FXML
     ChoiceBox<String> year;
 
+    private final int MIN_NAME_LENGTH = 4;
+    private final int MAX_NAME_LENGTH = 20;
+    private final int MIN_PASSWORD_LENGTH = 6;
+
+
     /**
      * Returns to the login scene if the user backs out of creating a new user.
      */
@@ -39,20 +44,20 @@ public class NewUserController extends SceneControllerParent implements Initiali
     }
 
     public void createUserClicked() {
-        String regex = "^[a-zA-Z0-9]+$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(username.getText());
-        if (username.getText().length() < 5 || username.getText().length() > 20) {
+        Matcher matcher = Pattern.compile("^[a-zA-Z0-9]+$").matcher(username.getText().trim());
+        String name = username.getText().trim();
+
+        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
             mainController.popUpWindow(Alert.AlertType.ERROR, "Användarnamnet är för kort eller långt", "Användarnamnet måste vara mellan 5 och 20 tecken långt");
         } else if(!matcher.matches()){
             mainController.popUpWindow(Alert.AlertType.ERROR, "Specialtecken är ej tillåtna", "Användarnamnet får ej innehålla specialtecken, som t ex !, &, *");
         } else if (!password.getText().equals(passwordRepeat.getText())) {
             mainController.popUpWindow(Alert.AlertType.ERROR, "Felaktigt lösenord", "Lösenorden du angav stämmer ej överens med varandra");
-        } else if (password.getText().length() < 6){
+        } else if (password.getText().length() < MIN_PASSWORD_LENGTH){
             mainController.popUpWindow(Alert.AlertType.ERROR, "Lösenordet är för kort", "Lösenordet måste vara minst 6 tecken långt");
         } else {
             String userYear = year.getValue();
-            NewUser user = new NewUser(username.getText(), password.getText());
+            NewUser user = new NewUser(name, password.getText());
             user.setYear(Integer.parseInt(userYear));
 
             HttpResponse<JsonNode> response = Unirest.post("http://localhost:5000/user").body(new Gson().toJson(user)).asJson();
